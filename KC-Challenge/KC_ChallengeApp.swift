565dbd97429a2b3e081118bb4c79b78b36ff9a33
Nihed Majdoub <majdoub.nihed@gmail.com>
2025-04-28 15:03:27 +0100

    Initial Commit

---
+++ KC-Challenge/KC_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KC_ChallengeApp.swift
+//  KC-Challenge
+//
+//  Created by Nihed Majdoub on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct KC_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KC-ChallengeTests/KC_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  KC_ChallengeTests.swift
+//  KC-ChallengeTests
+//
+//  Created by Nihed Majdoub on 28/04/2025.
+//
+
+import Testing
+@testable import KC_Challenge
+
+struct KC_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
