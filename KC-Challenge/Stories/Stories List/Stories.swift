22f8e0e0870c66ea4bc23057490b32196bef0a16
Nihed Majdoub <majdoub.nihed@gmail.com>
2025-05-05 22:23:57 +0200

    update stories tests

--- KC-Challenge/Stories/Stories List/Stories.swift
+++ KC-Challenge/Stories/Stories List/Stories.swift
@@ -24,7 +24,7 @@ struct Stories {
     case setStories([StoryModel])
     case setCurrentStory(StoryModel?)
     case setTimer
-    case timerTicked(Double)
+    case timerTicked
     case cancelTimer
     case closeButtonTapped
   }
@@ -35,7 +35,8 @@ struct Stories {
   private enum CancelID { case timer }
   
   private let storiesCount = 5
-  
+  private let interval = 0.2
+
   var body: some Reducer<State, Action> {
     Reduce { state, action in
       switch action {
@@ -62,15 +63,14 @@ struct Stories {
         state.timeElapsed = 0
         state.progress = 0
         
-        let interval = 0.2
         return .run { send in
           for await _ in clock.timer(interval: .seconds(interval)) {
-            await send(.timerTicked(interval), animation: .smooth)
+            await send(.timerTicked, animation: .smooth)
           }
         }
         .cancellable(id: CancelID.timer, cancelInFlight: true)
         
-      case .timerTicked(let interval):
+      case .timerTicked:
         guard let story = state.currentStory,
               story.duration >= state.timeElapsed else {
           return .run { await $0(.cancelTimer) }
--- KC-ChallengeTests/StoriesTests.swift
+++ KC-ChallengeTests/StoriesTests.swift
@@ -24,11 +24,43 @@ struct StoriesTests {
     store.exhaustivity = .off
     
     await store.send(.fetchStories)
-
+    
     await store.receive(\.setTimer) {
       $0.stories = StoryModel.mocks(count: 5)
       $0.currentStory = $0.stories.first
       $0.progress = 0
+    }
+  }
+  
+  @Test
+  func checkProgress() async throws {
+    let store = TestStore(initialState: Stories.State()) {
+      Stories()
+    } withDependencies: {
+      $0.continuousClock = ImmediateClock()
+    }
+    store.exhaustivity = .off
+    
+    await store.send(.fetchStories)
+    
+    await store.receive(\.timerTicked) {
+      $0.progress = $0.timeElapsed / $0.currentStory!.duration
+    }
+  }
+  
+  @Test
+  func setCurrentStory() async throws {
+    let store = TestStore(initialState: Stories.State()) {
+      Stories()
+    } withDependencies: {
+      $0.continuousClock = ImmediateClock()
+    }
+    store.exhaustivity = .off
+    let stories = StoryModel.mocks(count: 5)
+    await store.send(.setCurrentStory(stories.last))
+    
+    await store.receive(\.setTimer) {
+      $0.currentStory = stories.last
     }
   }
 }
